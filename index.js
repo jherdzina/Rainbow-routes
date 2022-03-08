2a01b330fd06c348896597de98dcf017415b2032
Jennifer Herdzina <jen@Jennifers-MacBook-Pro.local>
2022-03-07 20:40:58 -0600

    finished class assignment

---
+++ index.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send(`
+        <body style="margin: 0;">
+            <div style="border: 1px solid black; height: 10vh; background-color: white;">
+                <h2 style="text-align: center;">NAV BAR</h2>
+            </div>
+            <h1>White Page</h1>
+        </body>
+        `)
+})
+
+// Teal Page
+app.get('/:color', function (req, res) {
+    let myColor = req.params.color
+    res.send(`
+        <body style="margin: 0;">
+            <div style="border: 1px solid black; height: 10vh; background-color: ${myColor};">
+                <h2 style="text-align: center;">NAV BAR</h2>
+            </div>
+            <h1 style="color: ${myColor};">${myColor} Page</h1>
+        </body>
+    `)
+})
+
+
+
+app.listen(process.env.PORT)
